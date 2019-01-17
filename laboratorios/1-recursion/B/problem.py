a2f34237d851e9906beab64c8e34c342f61dc1f7
unknown <2153478@labinfo.is.escuelaing.edu.co>
2019-01-17 10:02:26 -0500

    ejercicio b

--- laboratorios/1-recursion/B/problem.py
+++ laboratorios/1-recursion/B/problem.py
@@ -2,8 +2,30 @@ import json
 
 
 # TODO Complete!
+def impar(numbers,c,d):
+    print("impar", d, c)
+    if c == len(numbers):
+        print(d)
+        return d
+    else:
+        if numbers[c]%2 == 1:
+            d.append(numbers[c])
+        return impar(numbers,c+1,d)
+def par(numbers,c,d):
+    print("par", d)
+    if c == len(numbers)-1:
+        c=0
+        return impar(numbers,c,d)
+    else:
+        if numbers[c]%2 == 0:
+            d.append(numbers[c])
+        return par(numbers,c+1,d)
+        
+    
 def arrange(numbers):
-    return numbers
+    c = 0
+    d = []
+    return par(numbers,c,d)
 
 
 if __name__ == '__main__':

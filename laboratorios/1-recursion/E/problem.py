4bc93cbdb988896211d3353d6189d26990df74f3
ViejoSantii <46783804+ViejoSantii@users.noreply.github.com>
2019-01-20 20:34:04 -0500

    Update problem.py

--- laboratorios/1-recursion/E/problem.py
+++ laboratorios/1-recursion/E/problem.py
@@ -1,9 +1,27 @@
 import json
 
-
+def super1(p,s):
+    if len(p)== 1:
+        s+=int(p[0])
+        p=s
+        return super2(str(p),s)
+    else:
+        s+=int(p[0])
+        
+        return super1(p[1:],s)
+def super2(p,s):
+    if len(p)==1:
+        return int(p[0])
+    else:
+        s=0
+        
+        return super1(p,s)
+    
 # TODO Complete!
 def super_digit(n, k):
-    return 0
+    p=n*k
+    s=0
+    return super1(p,s)
 
 
 if __name__ == '__main__':

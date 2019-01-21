a738bf7ebe8f45a76a2d91c5fc36f7aa4703231e
ViejoSantii <46783804+ViejoSantii@users.noreply.github.com>
2019-01-20 21:25:39 -0500

    Update problem.py
    La verdad, no supe solucionar el error de salida, pues revise todo el codigo y no se por que no me hace la comparacion correctamente.

--- laboratorios/1-recursion/C/problem.py
+++ laboratorios/1-recursion/C/problem.py
@@ -2,8 +2,40 @@ import json
 
 
 # TODO Complete!
+def cont(s,V,C):
+    if len(s)==0:
+        return result(V,C)
+    else:
+        if s[0]==("A"or"a"):
+            V=V+1
+            return cont(s[1:],V,C)
+        elif s[0]==("E"or"e"):
+            V=V+1
+            return cont(s[1:],V,C)
+        elif s[0]==("I"or"i"):
+            V=V+1
+            return cont(s[1:],V,C)
+        elif s[0]==("O"or"o"):
+            V=V+1
+            return cont(s[1:],V,C)
+        elif s[0]==("U"or"u"):
+            V=V+1
+            return cont(s[1:],V,C)
+        else:
+            C=C+1
+            return cont(s[1:],V,C)
+def result(V,C):
+    print(V,C)
+    if V>C:
+        return True
+    elif V<C:
+        return False
+    
 def has_more_vowels(s):
-    return False
+    V=0
+    C=0
+    print(s)
+    return cont(s,V,C)
 
 
 if __name__ == '__main__':

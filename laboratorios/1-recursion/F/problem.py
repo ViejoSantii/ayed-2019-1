5adac1dd8c960f0cf6061ba7ad0f280cfb4aa0f7
ViejoSantii <46783804+ViejoSantii@users.noreply.github.com>
2019-01-20 19:26:58 -0500

    Update problem.py

--- laboratorios/1-recursion/F/problem.py
+++ laboratorios/1-recursion/F/problem.py
@@ -1,9 +1,18 @@
 import json
+from sys import stdin
+def hanoi1(n ,inicio, aux, final):
+    if n==1:
+        print(inicio, "->", final)
+    else:
+        hanoi1(n-1,inicio, final, aux)
+        print(inicio, "->", final)
+        hanoi1(n-1, aux, inicio, final)
+
 
 
 # TODO Complete!
 def hanoi(n):
-    return []
+    return hanoi1(n, "A", "B", "C")
 
 
 if __name__ == '__main__':
